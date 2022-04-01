5f9cd71a471be84d0d4241c977bf5cb44abef1a7
leonphoenix21 <scamonoel169@gmail.com>
2022-04-01 13:04:45 -0500

    Demo-User-Auth-Complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,49 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
 
 
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
 
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 
 
